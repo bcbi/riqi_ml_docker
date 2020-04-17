21d59c499b8d492598f8140ee6ef84934cf95ac1
Dilum Aluthge <dilum@aluthge.com>
2020-04-17 16:21:57 +0000

    Update build.jl

--- build.jl
+++ build.jl
@@ -6,17 +6,20 @@ cd("riqi_ml")
 
 pkgs = [
     (name = "Econometrics", version = "0.2.6"), 
-    (name = "MCMCChains", version = "3.0.12"), 
+    # (name = "MCMCChains", version = "3.0.12"), 
     # (name = "Plots", version = "0.28.4"), 
-    (name = "PredictMD", version = "0.34.16"), 
-    (name = "PredictMDExtra", version = "0.13.12"), 
-    (name = "PredictMDFull", version = "0.13.7"), 
+    # (name = "PredictMD", version = "0.34.16"), 
+    # (name = "PredictMDExtra", version = "0.13.12"), 
+    # (name = "PredictMDFull", version = "0.13.7"), 
     # (name = "StatsPlots", version = "0.13"), 
-    (name = "Turing", version = "0.9.2"),
+    # (name = "Turing", version = "0.9.2"),
 ]
 
 no_test = String[
     "MCMCChains",
+    "PredictMD",
+    "PredictMDExtra",
+    "PredictMDFull"
     "Turing",
 ]
 
