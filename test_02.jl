09f6aa57ca380697f6eb55b7640d7b4a21a11493
Dilum Aluthge <dilum@aluthge.com>
2020-04-18 01:18:37 +0000

    Add UnicodePlots

--- test_02.jl
+++ test_02.jl
@@ -1,3 +1,3 @@
-run(`docker run aluthge/riqi_ml "JULIA_DEBUG=all PREDICTMD_TEST_GROUP=all PREDICTMD_TEST_PLOTS=true /usr/bin/julia -e 'import Clustering; import Econometrics; import MCMCChains; import MultivariateStats; import Plots; import PredictMD; import PredictMDExtra; import PredictMDFull; import StatsPlots; import Turing; '"`)
+run(`docker run aluthge/riqi_ml "JULIA_DEBUG=all PREDICTMD_TEST_GROUP=all PREDICTMD_TEST_PLOTS=true /usr/bin/julia -e 'import Clustering; import Econometrics; import MCMCChains; import MultivariateStats; import Plots; import PredictMD; import PredictMDExtra; import PredictMDFull; import StatsPlots; import Turing; import UnicodePlots; '"`)
 
 run(`docker run aluthge/riqi_ml "JULIA_DEBUG=all PREDICTMD_TEST_GROUP=all PREDICTMD_TEST_PLOTS=true /usr/bin/julia -e 'import Pkg; Pkg.test(string(:PredictMDExtra)); Pkg.test(string(:PredictMDFull)); Pkg.test(string(:PredictMD))'"`)
