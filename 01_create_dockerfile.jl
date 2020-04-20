d82e8e810454417d10897aa36ce752b9c8e4a674
Dilum Aluthge <dilum@aluthge.com>
2020-04-20 01:44:02 +0000

    Add minimum versions for all packages

--- 01_create_dockerfile.jl
+++ 01_create_dockerfile.jl
@@ -7,18 +7,18 @@ cd("riqi_ml")
 pkgs = [
     (name = "Clustering", version = "0.14.0 - *", ),
     (name = "Econometrics", version = "0.2.6 - *", ),
-    (name = "GR", ),
+    (name = "GR", version = "0.48.0 - *", ),
     (name = "MCMCChains", version = "3.0.12 - *", ),
     (name = "MultivariateStats", version = "0.7.0 - *", ),
-    (name = "PlotUtils", ),
-    (name = "Plots", ),
+    (name = "PlotUtils", version = "0.6.5 - *", ),
+    (name = "Plots", version = "0.29.9 - *", ),
     (name = "PredictMD", version = "0.34.17 - *", ),
     (name = "PredictMDExtra", version = "0.13.15 - *", ),
     (name = "PredictMDFull", version = "0.13.7 - *", ),
-    (name = "RecipesBase", ),
-    (name = "StatsPlots", ),
+    (name = "RecipesBase", version = "0.8.0 - *", ),
+    (name = "StatsPlots", version = "0.14.5 - *", ),
     (name = "Turing", version = "0.11.0 - *", ),
-    (name = "UnicodePlots", ),
+    (name = "UnicodePlots", version = "1.2.0 - *", ),
 ]
 
 no_test = String[
